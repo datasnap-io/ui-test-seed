737aa8012ab14427a9bf5e4787b11ffe96a16d8c
Derek Barnhart <derekbarnhart@gmail.com>
2015-03-03 14:41:06 -0800

    remove unneeded service

--- routes/weather.js
+++ routes/weather.js
@@ -17,16 +17,4 @@ router.get('/current', function(req, res) {
     })
 });
 
-//A simple proxy for forecast.io weather forecast service
-router.get('/forecast', function(req, res) {
-      var weatherPath = 'https://api.forecast.io/forecast/'+apiKey+'/37.8267,-122.423'
-    https.get(weatherPath,function(weatherRes){
-        weatherRes.pipe(res)
-    })
-    .on('error',function(e){
-        console.log(arguments)
-        res.status(500).json(e)
-    })
-});
-
 module.exports = router;
