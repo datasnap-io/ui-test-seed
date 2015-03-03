4b0f0651f26bd79f703003da2ee5ec122653ce7a
Derek Barnhart <derekbarnhart@gmail.com>
2015-03-03 11:26:44 -0800

    more boilerplate for app

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+angular.module('ds.weatherApp',['ui.router'])
+.config(function( $stateProvider, $url){
+
+    $stateProvider.
+        state('home',{
+            url:'/',
+            controller:'HomeController',
+            templateUrl:'./templates/HomeTemplate.html'
+        })
+
+})
+.controller('HomeController'function(){
+    console.log('Hey I am the home controller');
+})
+
