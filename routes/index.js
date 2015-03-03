d05ac456d9895543b3594e4ddc796b417f943af8
Derek Barnhart <derekbarnhart@gmail.com>
2015-03-03 12:02:35 -0800

    setup config and api key boilerplate

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    config = require('config');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Datasnap Weather App' });
+
+  res.render('index', {
+        title: 'Datasnap Weather App'
+        apiKey: config.forecastIOApiKey
+    });
 });
 
 module.exports = router;
